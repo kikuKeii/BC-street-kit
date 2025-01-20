df5648dc0184f1a76c1ef3b59110159675f3b6c3
kikukeii <miftakhuddinfalaki@gmail.com>
2025-01-20 10:12:27 +0700

    <fix>: Update log_write configuration to default to false

--- app/Config.js
+++ app/Config.js
@@ -3,5 +3,5 @@ const { HOST, PORT, LOG_WRITE } = process.env;
 module.exports = {
   host: HOST || "localhost",
   port: Number(PORT) || 3000,
-  log_write: LOG_WRITE || true,
+  log_write: LOG_WRITE || false,
 };
